16b69fb83c2e395368aeff1241e3d0a1365e6af1
Дуванова Ирина <vologjanochka@yandex.ru>
2026-04-27 16:42:56 +0300

    Initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="page">
+      <h1>My Photo</h1>
+      <img className="photo" src="/myfoto.jpg" alt="My photo" />
+    </main>
+  )
+}
+
+export default App
