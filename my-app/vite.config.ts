b7054caf6331a9ea7528bdd974dcf17cc74dba47
Дуванова Ирина <vologjanochka@yandex.ru>
2026-04-29 09:27:09 +0300

    Update local dev server config

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+    open: true,
+  },
 })
